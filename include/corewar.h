41e77ff2e55b7e83afc81c32612d3038e74591e0
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2025-04-25 11:33:52 +0200

    [fix] coding style op.h

---
+++ include/corewar.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** Corewar
+** File description:
+** Header file for the Corewar project
+*/
+
+#ifndef COREWAR_H_
+    #define COREWAR_H_
+
+    #include "op.h"
+
+typedef struct champion_s {
+    int reg[REG_NUMBER];
+} champion_t;
+
+#endif /* COREWAR_H_ */
