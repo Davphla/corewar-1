74cc86a75c3b18a6f6cdc0845f1e42014a34c2f5
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2025-04-30 09:38:59 +0200

    [fix] coding style

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -11,7 +11,6 @@ void war_correct_nb(war_t *war)
 {
     int index_champ = war->nb_champ;
 
-    printf("index champ = %i\n", index_champ);
-    for (int i = 0; i < index_champ; i ++)
+    for (int i = 0; i < index_champ; i++)
         free(war->champs[i + index_champ]);
 }
