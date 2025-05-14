0b80d4743cdc652ad9556c58fa0c7802fbc08f3f
Alexandre Kubiaczyk <alexandre.kubiaczyk@epitech.eu>
2025-05-14 11:18:23 +0200

    [Merge] merge conflict

--- src/corewar.c
+++ src/corewar.c
@@ -16,7 +16,7 @@ int corewar(war_t *war)
         update_vm(war);
         if (check_winner(war))
             break;
-        if (war->dump != -1 && war->cycle % war->dump == 0) {
+        if (war->dump != -1 && war->cycle % war->dump == 0)
             dump(war->vm);
     }
     display_winner(war);
